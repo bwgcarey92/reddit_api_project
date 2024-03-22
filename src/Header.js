b5ef7f4c9d3e3761e4f4a2d92ae6b70350a48a5d
Blake Carey <151703120+slbcarey@users.noreply.github.com>
2024-03-22 18:33:54 -0400

    created header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div>
+            <div>
+                <img />
+                <h2>$KR0LIT</h2>
+            </div>
+        </div>
+    );
+};
