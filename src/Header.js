bf1a3faf19f6753b9c8078b8821df105f825e6d6
Blake Carey <151703120+slbcarey@users.noreply.github.com>
2024-03-22 20:59:24 -0400

    added font, logo, styles to header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { FaRedditAlien } from "react-icons/fa";
 
 export default function Header() {
     return (
-        <div>
-            <div>
-                <img />
-                <h2>$KR0LIT</h2>
+        <div className="header">
+            <div className="logo-div">
+                <FaRedditAlien className="logo-img"/>
+                <h2 className="logo-title">$KR0LIT</h2>
             </div>
         </div>
     );
